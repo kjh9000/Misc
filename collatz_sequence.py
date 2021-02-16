bc0dda837ca15f7d98d0a40c2a2fdec84ac7d0a9
kevin <kjh9000@mail.com>
2021-02-15 17:31:52 -0800

    Collatz Sequence

---
+++ collatz_sequence.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+###############################################################################
+#                      collatz_sequence.py                                  #
+#                       github.com/kjh9000                                    #
+###############################################################################
+
+print("Please enter a number.")
+
+def collatz():
+    try:
+        number=int(input())
+        while number != 1:
+            if number % 2 == 0:
+                print(number)
+                number = number //2
+                continue
+            if number % 2 != 0:
+                print(number)
+                number = number * 3 + 1
+                continue
+    except ValueError:
+        print("This program accepts whole numbers only.")
+collatz()
