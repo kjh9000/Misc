68b23e088ba039863382cfdbb6403c6693684d08
kevin <kjh9000@mail.com>
2021-03-30 10:44:15 -0700

    A progrma to quickly calculate data usage for Tello users

--- datausecalc.py
+++ datausecalc.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
 
-# datausecalc.py
+# datausecalc.py -  A data usage calculator for Tello users
 # kjh9000@github.com
 
-'''My ISP only gives me the data used for each day, not the total for the 
-cycle. This program extracts the data used from the wall of text provided,
-adds it and returns the sum and the number of days into the plan one is.
+''' Instructions - Copy and paste the wall of text rom the website and paste it 
+into datausecalc.txt. Note the potential for fencepost errors. You are responsible for choosing what days to include in the calculation.
 '''
 
 import re
 
-testtext= '''
-# Begin edit
-
-Replace this wall of example text with your own. In the example, the cycle
-starts on 03/03/2021, and the current day is 03/09/2021. Double check what
-days are used in the calculation, since this is prone to fence post errors
-that affect the days count.
-
-03/09/2021 			18.85 MB 	Custom Plan 	-
-03/08/2021 			630.98 MB 	Custom Plan 	-
-03/07/2021 			654.05 MB 	Custom Plan 	-
-03/06/2021 			717.54 MB 	Custom Plan 	-
-03/05/2021 			939.73 MB 	Custom Plan 	-
-03/04/2021 			660.44 MB 	Custom Plan 	-
-03/03/2021 			1014.72 MB 	Custom Plan 	-
-# End of edit
-'''
-
-# extracts the data and adds it, then returns the sum along with the days
-sum = 0
-count = 0
-for i in re.compile(r'''
-    (\d{1,5}\.\d{1,2}) # This currently will search for data up to not including 10GB per day
-    ''', re.VERBOSE).findall(testtext):
-    count += 1
-    sum += float(i)
-print("You have used about", round(sum / 1000, 2), "GB of data in", count, "days.")
+try:
+    testtext = open('datausecalc.txt')
+except (NameError, FileNotFoundError):
+    print("This program requires a datausecalc.txt file. You can either create it or download it.")
+else:
+    sum = 0
+    count = 0
+    for i in re.compile(r'''
+        (\d{1,5}\.\d{1,2})
+        ''', re.VERBOSE).findall(testtext.read()):
+        count += 1
+        sum += float(i)
+    print("You have used about", round(sum, 2), "MB of data in", count, "days.")
