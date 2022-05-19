a132b1b548a62e707e4779ea353b84be65adde75
kevin <kjh9000@mail.com>
2022-05-19 08:06:05 -0700

    A simple hangman game

--- hangman.py
+++ hangman.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-# hangman.py
+# hangman.py v2
 # github.com/kjh9000
 
 import random
 
-# you can extend the word list. 
+# you can extend the word list here. 
 wordlist = ['california', 'programming', 'washington', 'multiplication']
-mysteryword = wordlist[random.randint(0,len(wordlist))]
 
 
+mysteryword = wordlist[random.randint(0,len(wordlist)-1)]
 obscuredword = "_" * len(mysteryword)
 hangman  = ""
 parts = ['  |  \n', '  0\n', ' /', '|', '\\\n', ' /',' \\']
 while parts:
+    display = " ".join(list(obscuredword))
     if "_" not in obscuredword:
         print("You win! Game over.")
         break
-    print("the word is %s." % obscuredword)
-    guess = input("Give me a lowercase letter: ")
+    print("the word is %s." % display)
+    guess = input("Give me a lowercase letter: ").lower()
     if guess in obscuredword:
         print("You already guessed %s." % guess)
+    elif len(guess)>1:
+        print("Letters can only be entered one at a time.")
     elif guess in mysteryword:
         print("The letter %s in in the mystery word" % guess)
         temp = ""
